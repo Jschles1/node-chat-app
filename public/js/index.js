9ae373cef975b882e06b7e7dde2047f0dd2e6374
Jschles1 <jschles1@ramapo.edu>
2018-03-12 21:54:25 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,16 @@ const socket = io();
 socket.on('connect', function () {
   console.log('Connected to Server');
 
-  socket.emit('createMessage', {
-    from: 'Mike',
-    text: 'Hey'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'Mike',
+  //   text: 'Hey'
+  // });
 });
 
 socket.on('disconnect', function () {
   console.log('Server Disconnected');
 });
 
-socket.on('newMessage', function (msg) {
-  console.log('New Message: ', msg);
+socket.on('newMessage', function (message) {
+  console.log('New Message: ', message);
 });
--- server/server.js
+++ server/server.js
@@ -15,14 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User Connected');
 
-  socket.emit('newMessage', {
-    to: 'John',
-    text: 'Hello',
-    createdAt: 123
-  });
+  // socket.emit('newMessage', {
+  //   from: 'John',
+  //   text: 'Hello',
+  //   createdAt: 123
+  // });
 
-  socket.on('createMessage', (newMsg) => {
-    console.log('createMessage: ', newMsg);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage: ', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
